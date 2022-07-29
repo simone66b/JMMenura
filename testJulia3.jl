d0b62a804aa2c1af04eecffd399541d21cf5a905
simone66b <s.blomberg1@uq.edu.au>
2022-07-29 13:28:34 +1000

    evolving MV OU along a tree

--- testJulia3.jl
+++ testJulia3.jl
@@ -240,21 +240,6 @@ theRoot.other[1].data["1"] = rand(10)
 
 using DifferentialEquations, Phylo, Plots, Distributions, StatsPlots; pyplot()
 
-alpha_vec = [3.0, 3.0, 4.0, 2.0]
-
-mu_vec = [5.843333, 3.057333, 3.758000, 1.199333]
-sigma_vec = [1.0, 1.0, 1.0,  1.0]
-
-p = [alpha_vec, mu_vec, sigma_vec]
-function f(du, u, p, t)
-    alpha , mu, sigma = p
-    du .= alpha .* (mu - u)
-end # f
-
-function g(du, u, p, t)
-    alpha, mu, sigma = p
-    du .= sigma
-end # g
 
 # rho = 0.0
 # mat = [1.0 rho rho rho rho;
@@ -262,6 +247,8 @@ end # g
 #        rho rho 1.0 rho rho;
 #        rho rho rho 1.0 rho;
 #        rho rho rho rho 1.0]
+
+## Iris data:
 
 mat = [1.0000000  -0.1175698    0.8717538   0.8179411;
       -0.1175698   1.0000000   -0.4284401  -0.3661259;
@@ -274,6 +261,78 @@ tspan = (0.0, time_tot)
 OU_noise = CorrelatedWienerProcess(mat, tspan[1], zeros(4), zeros(4)) 
 
 u0 = mu_vec
+
+
+using DifferentialEquations, Phylo, Plots; pyplot()
+
+function mysim(tree, x0=0.0, t0 = 0.0)
+    ## define the OU simulation
+
+    function OU(x0, tspan)
+    function drift(du, u, p, t)
+        alpha , mu, sigma = p
+        du .= alpha .* (mu - u)
+    end # drift
+
+    function diff(du, u, p, t)
+        alpha, mu, sigma = p
+        du .= sigma
+    end # diff
+
+        dt = 0.001
+        alpha_vec = [3.0, 3.0, 4.0, 2.0]
+        mu_vec = [5.843333, 3.057333, 3.758000, 1.199333]
+        sigma_vec = [1.0, 1.0, 1.0,  1.0]
+
+p = [alpha_vec, mu_vec, sigma_vec]
+
+  prob = SDEProblem(drift, diff, u0,tspan, p=p, noise=OU_noise)       
+  sol = solve(prob, dt=dt, p=p, adaptive=false)   
+    end #OU
+    
+    function Recurse!(tree, node)
+        if ismissing(node.inbound) ## the root node, to get started
+            for i in 1:length(node.other) ## 'other' means branches
+                node.other[i].data["1"]  = ## "1" is a placeholder for the Dict
+                    OU(x0, (t0, getheight(tree, node.other[i].inout[2])));
+            end
+        else
+            for i in 1:length(node.other) ## NB 'other' are branches here!
+                node.other[i].data["1"] =
+                    OU(node.inbound.data["1"].u[end],
+                       (getheight(tree, node), getheight(tree, node) +
+                        node.other[i].length))
+            end
+        end
+        for i in 1:length(node.other)
+            if !isleaf(tree, node.other[i].inout[2])
+                Recurse!(tree, node.other[i].inout[2])
+            end
+        end
+    end# Recurse!
+    nodeInit = getroot(tree)
+    Recurse!(tree, nodeInit) # do the recursive simulations
+    tree
+end # mysim
+
+tr = Ultrametric(1024, 10.0);
+tree = rand(tr);
+plot(tree)
+## savefig("tree.png")
+
+test = mysim(tree);
+testbranches=getbranches(test);
+
+plot(xlim=[0.0, 10.0], ylim= [-0.1, 2.0], legend=nothing)
+for i in testbranches
+    plot!(i.data["1"].t, i.data["1"].u, legend= nothing)
+end
+current()
+savefig("trace.png")
+
+
+
+
 
 prob = SDEProblem(f,g, u0,tspan, p=p, noise=OU_noise)
 dt = 1e-3
