09dfcc4fa49fa17fb6b80637f656ab9130dbfe10
Simone <s.blomberg1@uq.edu.au>
2022-08-14 15:53:31 +1000

    code cleaning

--- testJulia3.jl
+++ testJulia3.jl
@@ -13,11 +13,17 @@ time_tot = 1.0
 tspan = (0.0, time_tot)
 x0 = [5.843333, 3.057333, 3.758000, 1.199333] ## actual iris means
 
-## x0 = [0.5, 0.5, 0.5, 0.5]
+## tree = open(parsenewick, "tree.phy") Use if reading tree in from file
+## We will create a random tree instead.
 
-function mysim(tree, x0, mat, t0 = 0.0)
-    ## define the OU simulation
-    function OU(x0, tspan)
+alpha_vec = [3.0, 1.0, 2.0, 3.0]
+mu_vec = [5.0, 3.0, 2.0, 3.0]
+sigma_vec = [1.0, 1.0, 2.0, 3.0]
+
+## x0 = [0.5, 0.5, 0.5, 0.5]
+p = [alpha_vec, mu_vec, sigma_vec]
+
+function diffusion(du, u, p, t)
         function drift(du, u, p, t)
             alpha, mu, sigma = p
             ## du .= alpha .* u would be BM with drift
@@ -29,34 +35,25 @@ function mysim(tree, x0, mat, t0 = 0.0)
             ## du .= sigma would be OU
             du .= sqrt.(u) .* sigma ## Cox-Ingersoll-Ross Gamma model
         end # diff
+end # diffusion
 
-        dt = 0.001
-        alpha_vec = [3.0, 1.0, 2.0, 3.0]
-        mu_vec = [5.0, 3.0, 2.0, 3.0]
-        sigma_vec = [1.0, 1.0, 2.0, 3.0]
+function menura(tree, x0, mat, P, t0 = 0.0, dt=0.001 )
 
-        p = [alpha_vec, mu_vec, sigma_vec]
-        OU_noise = CorrelatedWienerProcess(mat, tspan[1],
-                                           zeros(length(alpha_vec)),
-                                           zeros(length(alpha_vec))) 
 
-        prob = SDEProblem(drift, diff, x0,tspan, p=p, noise=OU_noise)       
-        sol = solve(prob, dt=dt, p=p, adaptive=false)   
-    end #OU
     
     function Recurse!(tree, node)
         if ismissing(node.inbound) ## the root node, to get started
             for i in 1:length(node.other) ## 'other' means branches
                 node.other[i].data["1"]  = ## "1" is a placeholder for the Dict
-                OU(x0, (t0, getheight(tree, node.other[i].inout[2])));
+               diffusion(x0, (t0, getheight(tree, node.other[i].inout[2])));
             end
         else
             for i in 1:length(node.other) ## NB 'other' are branches here!
                 node.other[i].data["1"] =
-                    OU(node.inbound.data["1"].u[end],
+                    diffusion(node.inbound.data["1"].u[end],
                        (getheight(tree, node), getheight(tree, node) +
                         node.other[i].length))
-            end
+            end # for
         end
         for i in 1:length(node.other)
             if !isleaf(tree, node.other[i].inout[2])
@@ -64,19 +61,19 @@ function mysim(tree, x0, mat, t0 = 0.0)
             end
         end
     end# Recurse!
+
+    noise = CorrelatedWienerProcess(mat, tspan[1],
+                                    zeros(dim(mat)),
+                                    zeros(dim(mat))) 
+
+    prob = SDEProblem(drift, diff, x0,tspan, p=p, noise=noise)       
+    sol = solve(prob, dt=dt, p=p, adaptive=false) 
     nodeInit = getroot(tree)
     Recurse!(tree, nodeInit) # do the recursive simulations
     tree
-end # mysim
+end # menura
 
-## tree = open(parsenewick, "tree.phy") Use if reading tree in from file
-## We will create a random tree instead.
-
-tr = Ultrametric(5, 1.0);
-tree = rand(tr);
-plot(tree)
-
-test = mysim(tree, x0, mat);
+test = mysim(tree, x0, mat, P); ### P is priors
 
 #### get the last multivariate trait value in a branch
 testtips = collect(nodefilter(isleaf, test));
@@ -92,11 +89,21 @@ res[i] = testtips[i].inbound.data["1"].u[end];
     push!(tiptimes, testtips[i].inbound.data["1"].t[end]);
 end
 
-map((i, j) -> finaltraitvals[i] = j, tipnames, res);
-map((i, j) -> tiptimesdict[i] = j, tipnames, tiptimes);
+## map((i, j) -> finaltraitvals[i] = j, tipnames, res);
+## map((i, j) -> tiptimesdict[i] = j, tipnames, tiptimes);
+resvalsflat =  collect(Iterators.flatten(res))
+collect(Iterators.flatten(res))
+end # menura
+tiptimesdict
+
+
+tr = Ultrametric(100, 1.0);
+tree = rand(tr);
+### plot(tree)
 
 
 
+euclidean(vals, resvalsflat)
 ###########################3 Plotting ###################################3
 testbranches = getbranches(test);
 plot(xlim = (0.0,1.0), ylim = (0.0, 10.0), zlim=(0.0, 10.0), legend=nothing,
