945f64cff7e65858d61ff1fcca5919a9ba6b0e6a
Simone <s.blomberg1@uq.edu.au>
2022-09-29 19:08:36 +1000

    more failed covariance modelling

--- testJulia5.jl
+++ testJulia5.jl
@@ -1,17 +1,16 @@
 using DifferentialEquations, Phylo, Plots, Distributions, Statistics,
-    Distances, KissABC, JLD2, LinearAlgebra,
-    GeometricIntegratorsDiffEq, StochasticIntegrators; pyplot();
+    Distances, KissABC, JLD2, LinearAlgebra, pyplot();
 
 
         function drift(du, u, p, t)
             alpha, mu, sigma = p
             ## du .= alpha .* u would be BM with drift
-            du = alpha * (mu - u) ## OU-like
+            du .= alpha .* (mu .- u) ## OU-like
         end # drift
 
         function diff(du, u, p, t)
             alpha, mu, sigma = p
-            du = sigma ## would be OU
+            du .= sigma ## would be OU
             ## du .= sqrt.(u) .* sigma ## Cox-Ingersoll-Ross Gamma model
             ## du .= sqrt.(abs.(u .* (ones(length(sigma)) .- u))) .* sigma ## Beta model
       end # diff
@@ -29,10 +28,35 @@ p = (alpha1, mu1, sigma1)
 
 prob = SDEProblem(drift, diff, u0, tspan, p=p)
 
-sol = solve(prob,GIEuler())
-
-npar = 3 ## alpha mu, sigma of the SDE
-ndims = length(alpha1)
+sol = solve(prob, ImplicitEM(theta=1/2, symplectic=true), p=p, dt=0.001)
 
 
 
+
+
+   function v(t, q, v, p)
+          λ = p[:lambda]
+          v[1] = λ*q[1]
+          v[2] = λ*q[2]
+      end
+  
+      function B(t, q, B, p, col=0)
+          μ = p[:mu]
+          if col==0 #whole matrix
+              B[1,1] = μ*q[1]
+              B[2,1] = μ*q[2]
+          elseif col==1
+              #just first column
+          end
+      end
+  
+      t0 = 0.0
+      q0 = [1.0, 1.0]
+      lambda  = 2.0
+      mu  = 1.0
+      p = (lambda=lambda, mu=mu)
+  
+sde = SDE(1, 1, v, B, t0, q0; parameters=p)
+
+
+int = Integrator(sde, TableauImplicitEuler(), 0.001)
