013712b4a7c48347c234c9b586477e50a1e520b5
Simone <s.blomberg1@uq.edu.au>
2022-12-07 18:55:14 +1000

    graphing

--- testJulia4.jl
+++ testJulia4.jl
@@ -1,4 +1,4 @@
-using DifferentialEquations, Phylo, Plots, Distributions, Distances, KissABC, JLD2, LinearAlgebra; pyplot();
+using DifferentialEquations, Phylo, Plots, Distributions, Distances, JLD2, LinearAlgebra; pyplot();
 
     #####################################################################################
     #####################################################################################
@@ -140,8 +140,8 @@ end # gen_cov_mat
 
     a1=1.0;
     b1= 1.0;
-x0 =  [5.843333, 3.057333, 3.758000, 1.199333]
-tree1 = Ultrametric(100);
+x0 =  repeat([0.0], 8);
+tree1 = Ultrametric(20);
     tree = rand(tree1); 
        time_tot = 1.0
     tspan = (0.0, time_tot)
@@ -150,11 +150,16 @@ tree1 = Ultrametric(100);
     #    1.2743154  -0.3296564    3.1162779   1.2956094;
     #        0.5162707  -0.1216394    1.2956094   0.5810063];
 
-P0 = Diagonal([1, 1, 1, 1])
-mat = P0
-u0=zeros(size(mat))
+##P0 = Diagonal([1, 1, 1, 1])
 
-
+P0 = [0.329 0.094 -0.083 -0.089 0.293 0.079 0.208 0.268;
+0.094 0.449 0.349 0.24 0.071 0.075 0.03 0.009;
+-0.083 0.349 1.426 0.487 -0.371 -0.098 -0.053 -0.172;
+-0.089 0.24 0.487 0.546 -0.168 0.017 -0.051 -0.081;
+0.293 0.071 -0.371 -0.168 1.441 1.008 0.904 0.945;
+0.079 0.075 -0.098 0.017 1.008 1.087 0.731 0.78;
+0.208 0.03 -0.053 -0.051 0.904 0.731 0.809 0.783;
+0.268 0.009 -0.172 -0.081 0.945 0.78 0.783 0.949]
 
 # P0 = [1.54 0.26 1.14 0.26;
 #       0.26 0.46 0.26 0.06;
@@ -162,33 +167,43 @@ u0=zeros(size(mat))
 #       0.26 0.06 0.26 0.46]
 
 
+mat = P0
+u0=zeros(size(mat))
+
+alpha1 = repeat([1.0], 8);
+mu1 = repeat([0.0], 8) ## randn(8); ## Start at the trait means
+sigma1 = repeat([1.0], 8)
+
+# function simulate((alpha, mu, sigma))
+#     p1 = (alpha=alpha, mu=mu, sigma=sigma, mat=P0, a=a1, b=b1)
+#     putp!(tree, p1, "parameters");
+#     menuramat!(tree);
+#     menura!(tree);
+# (tree, predictTraitTree(tree))
+# end
 
 
-alpha1 = (3.0, 3.0, 3.0, 3.0);
-mu1 = (5.843333, 3.057333, 3.758000, 1.199333); ## Start at the trait means
-sigma1 = (1.0, 1.0, 1.0, 1.0);
-
-function simulate((alpha, mu, sigma))
-    p1 = (alpha=alpha, mu=mu, sigma=sigma, mat=P0, a=a1, b=b1)
+function simulate(tree, alpha=alpha1, sigma=sigma1, mu=mu1, mat=P0, a=a1, b=b1)
+    p1 = (alpha=alpha, mu=mu, sigma=sigma, mat=P0, a=a, b=b)
     putp!(tree, p1, "parameters");
     menuramat!(tree);
     menura!(tree);
 (tree, predictTraitTree(tree))
 end
 
-exampledat = simulate((alpha1, mu1, sigma1));
+exampledat = simulate(tree, alpha1, sigma1, mu1);
 
-priordists = [Truncated(Normal(0,3), 0, Inf)]
-priors = Factored(repeat(priordists, 12)...,)
+# priordists = [Truncated(Normal(0,3), 0, Inf)]
+# priors = Factored(repeat(priordists, 12)...,)
 
-function cost((alpha, mu, sigma))
-    x=simulate((alpha, mu, sigma))[2]
-    y=exampledat[2]
-    euclidean(x, y)
-end #cost
+# function cost((alpha, mu, sigma))
+#     x=simulate((alpha, mu, sigma))[2]
+#     y=exampledat[2]
+#     euclidean(x, y)
+# end #cost
 
-approx_density = ApproxKernelizedPosterior(priors,cost,0.005)
-## res = sample(approx_density, AIS(25), 1000, ntransitions=100, discard_initial = 10)   
+# approx_density = ApproxKernelizedPosterior(priors,cost,0.005)
+# ## res = sample(approx_density, AIS(25), 1000, ntransitions=100, discard_initial = 10)   
 
 ## save_object("ABCResults.jld2", res)
 
@@ -196,49 +211,50 @@ approx_density = ApproxKernelizedPosterior(priors,cost,0.005)
 ########################################################################
 #########################################################################
 
- plot(tree,
-    size = (400, 800),
-    markersize = 20, 
-    series_annotations = text.(1:nnodes(tree), 15, :center, :center, :white))
-current()
+#  plot(tree,
+#     size = (400, 800),
+#     markersize = 20, 
+#     series_annotations = text.(1:nnodes(tree), 15, :center, :center, :white))
+# current()
 
 testnodes = getnodes(exampledat[1]);
-    plot(xlim = (0.0,1.0), ylim = (4.0, 8.0), zlim=(0.0, 4.0), legend=nothing,
+
+    plot(xlim = (0.0,1.0), ylim = (-2.0, 2.0), zlim=(-2.0, 2.0), legend=nothing,
      reuse=false)
 for i in testnodes
     u1= i.data["trace"]
-    uu1 = transpose(reshape(collect(Iterators.flatten(u1)), 4, length(u1)))
+    uu1 = transpose(reshape(collect(Iterators.flatten(u1)), 8, length(u1)))
     myt = i.data["timebase"]
     plot!(myt, uu1[:, 1], uu1[:,2])
 end
 current()
 
-plot(xlim=(0.0,1.0), ylim= (2.0, 6.0), legend=nothing, reuse=false)
-for i in testnodes
-    u1= i.data["trace"]
-    uu1 = transpose(reshape(collect(Iterators.flatten(u1)), 4, length(u1)))
-    myt = i.data["timebase"]
-    plot!(myt, uu1[:, 3])
-end
-current()
+# plot(xlim=(0.0,1.0), ylim= (2.0, 6.0), legend=nothing, reuse=false)
+# for i in testnodes
+#     u1= i.data["trace"]
+#     uu1 = transpose(reshape(collect(Iterators.flatten(u1)), 8, length(u1)))
+#     myt = i.data["timebase"]
+#     plot!(myt, uu1[:, 3])
+# end
+# current()
 
-plot(xlim=(2.0,6.0), ylim= (4.0, 8.0), legend=nothing, reuse=false)
+plot(ylim=(-2.0, 2.0), xlim= (-2.0, 2.0), legend=nothing, reuse=false)
 for i in testnodes
     u1= i.data["trace"]
-    uu1 = transpose(reshape(collect(Iterators.flatten(u1)), 4, length(u1)))
+    uu1 = transpose(reshape(collect(Iterators.flatten(u1)), 8, length(u1)))
     plot!(uu1[:,3], uu1[:, 1])
 end
 current()
 
-plot(xlim=(2.0,6.0), ylim= (4.0, 8.0), zlim=(0.0, 5.0), legend=nothing,
-     reuse=false)
-for i in testnodes
-    u1= i.data["trace"]
-    uu1 = transpose(reshape(collect(Iterators.flatten(u1)), 4, length(u1)))
-    myt = i.data["timebase"]
-    plot!(uu1[:,3], uu1[:, 1], uu1[:,2])
-end
-current()
+# plot(xlim=(2.0,6.0), ylim= (4.0, 8.0), zlim=(0.0, 5.0), legend=nothing,
+#      reuse=false)
+# for i in testnodes
+#     u1= i.data["trace"]
+#     uu1 = transpose(reshape(collect(Iterators.flatten(u1)), 8, length(u1)))
+#     myt = i.data["timebase"]
+#     plot!(uu1[:,3], uu1[:, 1], uu1[:,2])
+# end
+# current()
 
 
 
