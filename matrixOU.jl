0ed679649c7d08299513bdd25eb5bb51f6f6278e
Simone <s.blomberg1@uq.edu.au>
2023-07-15 22:12:52 +1000

    tweak

--- matrixOU.jl
+++ matrixOU.jl
@@ -80,7 +80,7 @@ end # OUmatrix function
 
 
 ###################################################################################################3
-### FIX THIS 
+
 mu1 = 1000 .* FullData[1,:Gmatrix]
 
 alpha = fill(4.0, size(mu1, 1), size(mu1, 1))
@@ -101,43 +101,4 @@ vals = map(x -> x[i,j], test2)
 plot!(vals, legend=false)
     end
 end
-<<<<<<< HEAD
 current()
-=======
-current()
-
-
-##################################################3########
-
-############# Data entry ############################3####
-
-using CSV, Tables, DataFrames, XLSX, Statistics, Pipe
-
-cd("/home/simoneb/Desktop/JMMenura") ## may need to change this to suit
-nms = ["cris.txt", "ever.txt", "grah.txt",
-"line.txt", "pulc.txt", "sagr.txt", "smar.txt"];
-matmatrix = CSV.File(nms, header=0) |> Tables.matrix
-Garray = reshape(matmatrix', 8,8,7)
-Gvec = [Garray[1:8, 1:8, i] for i in 1:7] ## Species Gmatrices
-DFmats = DataFrame(Gmatrix=Gvec)
-
-xf = DataFrame(XLSX.readtable("Adult measurements for divergence.xlsx", 
-"Pmatrix Measurements with outli"))
-
-vecnames = append!([2], 4:11) # columns to keep
-AnolesData = @pipe xf[:,vecnames] |> 
-filter(row -> row.Species .== "A. cristatellus" || 
-row.Species .== "A. evermanni" ||
-row.Species .== "A. grahami" ||
-row.Species .== "A. lineatopus" ||
-row.Species .== "A. pulchellus" ||
-row.Species .== "A. sagrei" ||
-row.Species .== "A. smaragdinus", _) |>
-groupby(_, :Species) |>
-combine(_, 2:9 .=> (x -> mean(skipmissing(x)))) |> ## species means
-transform(_, 2:9 .=> (x -> log.(x))) |>
-select(_, Not(2:9)) |>
-rename(_, names(xf)[vecnames])
-
-FullData = hcat(AnolesData, DFmats)
->>>>>>> main
