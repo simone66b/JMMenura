5558e9020870b4be9c222557f265f4ac9e7b422d
j-Cooper-A <Cooper.Janke@gmail.com>
2023-08-14 10:15:07 +1000

    Adding some more test files

--- tests/test_floating_point_probs.jl
+++ tests/test_floating_point_probs.jl
@@ -1,4 +1,4 @@
-# Test file to test the use of different diffusion models
+# Test file to test floating point errors
 using .JMMenura
 using Phylo, Distributions, Random, Plots, LinearAlgebra
 
@@ -19,7 +19,11 @@ time_tot = 1.0
 tspan = (0.0, time_tot)
 
 # G matrix
+Random.seed!(4)
 P0 = cor(rand(Wishart(100, Matrix(1I, n, n)  ))) # Change to Wishart Distribution
+
+# Looking for small negative eigenvalue
+eigen(P0)
 
 # trait vairables - note alpha*mu >= sigma
 alpha1 = repeat([1.0], n)
@@ -32,13 +36,18 @@ mat_alpha = 0 .* ones(n,n)
 mat_sigma = (1 / sqrt(2) * 0.10) .* ones(n,n)
 mat_mu = copy(P0)
 
-# testing the cox ingersoll ross gamma model
-data1 = menura_sim_mat_OU_each(alpha1, sigma1, mu1, P0, mat_alpha, mat_sigma, mat_mu, tree1, 
-trait_diff = trait_diff_cox_ingersoll_ross_gamma)
+# This should error by an inexact error
+data1 = menura_sim_mat_OU_each(alpha1, sigma1, mu1, P0, mat_alpha, mat_sigma, mat_mu, tree1)
 
-plot_data(data1[1], 1, 2)
+# Adding a small error value
+ϵ = Matrix((10^-15)I, n, n)
+P1 = P0 + ϵ
+mat_mu1 = copy(P1)
 
-# testing the usual model
-data2 = menura_sim_mat_OU_each(alpha1, sigma1, mu1, P0, mat_alpha, mat_sigma, mat_mu, tree2)
+# Check eigenvalues
+eigen(P1)
+
+# Should not error now
+data2 = menura_sim_mat_OU_each(alpha1, sigma1, mu1, P1, mat_alpha, mat_sigma, mat_mu1, tree1)
 
 plot_data(data2[1], 1, 2)
