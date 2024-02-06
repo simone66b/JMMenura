d70783780c5017bb8992584d7963a849fdef1b0a
Simone <s.blomberg1@uq.edu.au>
2024-02-06 17:23:34 +1000

    More OU

--- matrixOUSB.jl
+++ matrixOUSB.jl
@@ -6,27 +6,20 @@ cd("/home/simoneb/Desktop/JMMenura") ## may need to change this to suit
 
 #############################################################################
 function drift(du, u, p, t)
-    du .= p.alpha .* logMap(Fisher, Hermitian(u), Hermitian(p.mu))# Mean reversion
+  du .= p.alpha * inv(sqrt(Hermitian(u, :L))) * Hermitian(p.mu, :L) * inv(sqrt(Hermitian(u, :L)))# Mean reversion
+  ##println(du)
 end  ## drift function
 
 function diffusion(du, u, p, t)
-    du .= p.sigma  ## scaled BM
+  du .= p.sigma  ## scaled BM
 end ## diffusion function
 
-function OUmatrix(drift, diffusion, G0, mu, alpha, sigma, tspan, dt)
-temp = Matrix{Float64}[]
-push!(temp, G0)
-pp = (mu = mu, G0=G0, alpha =  alpha, sigma = sigma) ## tuple of parameters
-
-prob = SDEProblem(drift, diffusion, G0, tspan, p = pp) ## set up the sde problem
-sol = solve(prob, EM(), p = pp, dt = dt) ## solve using Euler-Maruyama
-G1 = expMap(Fisher, Hermitian(sol.u[2]), Hermitian(sol.u[1])) # back transfer trace onto manifold
-push!(temp, G1)
-  ## can now plot, print this, etc.
+function OUmatrix(drift, diffusion, G0, mu, alpha, sigma, tspan, dt, W)
+  pp = (mu = mu, G0=G0, alpha =  alpha, sigma = sigma) ## tuple of parameters
+  prob = SDEProblem(drift, diffusion, G0, tspan, p = pp, noise=W, noise_rate_prototype=zeros(size(G0))) ## set up the sde problem
+  sol = solve(prob, EM(), p = pp, dt = dt) ## solve using Euler-Maruyama
+sol
 end # OUmatrix function
-
-
-
 
 ###################################################################################################3
 
@@ -55,29 +48,40 @@ tree = open(parsenewick, Phylo.path("/home/simoneb/Desktop/JMMenura/pruned7.tre"
 ## display(plot(tree, linewidth=5, tipfont=20))
 
 G0Vec = [0.285, 0.118, 0.131, 0.053, 0.212, 0.172, 0.188, 0.210, 0.277,
-            0.230, 0.103, 0.074, 0.073, 0.045, 0.023, 0.800, 0.317, 0.054,
-            0.121, 0.152, 0.147, 0.654, 0.028, 0.047, 0.124, 0.148, 0.858,
-            0.749, 0.581, 0.626, 0.906, 0.553, 0.644, 0.708, 0.731, 0.872]
+         0.230, 0.103, 0.074, 0.073, 0.045, 0.023, 0.800, 0.317, 0.054,
+         0.121, 0.152, 0.147, 0.654, 0.028, 0.047, 0.124, 0.148, 0.858,
+         0.749, 0.581, 0.626, 0.906, 0.553, 0.644, 0.708, 0.731, 0.872]
 
-            dt = 0.001
+dt = 0.001
 G0 =zeros(8, 8)
 idx = tril!(trues(size(G0)))
 G0[idx] .= G0Vec
-G0 .= Hermitian(G0, :L)
+G0 = Hermitian(G0, :L)
 ## u0 = G0 #### FullData[7, :Gmatrix]; ## randPosDefMat(8)
 mu = G0
-alpha = fill(2.0, size(G0, 1), size(G0, 1));
-sigma = fill(1 / sqrt(2), size(G0, 1), size(G0, 1));
-sigma[diagind(sigma)] .= 1;
+alpha = 2.0 ###fill(2.0, size(G0, 1), size(G0, 1));
+sigma = fill(1.0 / sqrt(2.0), size(G0, 1), size(G0, 1));
+sigma[diagind(sigma)] .= 1.0;
+## sigma = 0.05 ##  .* sigma
 tspan = (0.0, 0.001)
+W=CorrelatedWienerProcess!(sigma, tspan[1], G0)
 
-@time test = OUmatrix(drift, diffusion, G0, mu, alpha, sigma, tspan, dt);
-#= # Extract the elements from the matrices
-test2 = convert(Vector{Matrix{Float64}}, test);
+temp = Matrix{Float64}[]
+push!(temp, G0)
+for i = 2 ## :1000
+  test = OUmatrix(drift, diffusion, temp[i-1], mu, alpha, 0.2, tspan, dt, W)
+  G1 = sqrt(Hermitian(temp[i-1],:L)) * exp(Hermitian(test.u[1], :L)) * sqrt(Hermitian(temp[i-1],:L))
+  println(G1)
+  push!(temp, G1) 
+end
+
+temp2 = convert(Vector{Matrix{Float64}}, temp)
+# Extract the elements from the matrices
+## test2 = convert(Vector{Matrix{Float64}}, test);
 ## test2 .= cov2cor.(test2)  ## uncomment to see correlation matrix plots
-vals = map(x -> x[tril(trues(size(x)))], test2); 
+vals = map(x -> x[tril(trues(size(x)))], temp2); 
 plot(reuse = false) ## extract lower triangular from test
 for i in 1:Integer(dim(test[1], 1) * (dim(test[1], 1) - 1) / 2 + dim(test[1], 1))
-plot!(map(x -> x[i], vals), legend = false) =#
+plot!(map(x -> x[i], vals), legend = false)
 end
-## current()
+current()
