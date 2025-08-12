2f5c2ed36d82ea1c168aff8e7cc12e680df520cf
Simone <s.blomberg1@uq.edu.au>
2025-08-12 18:50:59 +1000

    simulation updates

--- paper_scripts/importance_simulated_scripts/simulated/BM testdata/simulated_importance_BMBM_larger_trait_3_model_run.jl
+++ paper_scripts/importance_simulated_scripts/simulated/BM testdata/simulated_importance_BMBM_larger_trait_3_model_run.jl
@@ -1,11 +1,11 @@
 using Pkg
 ## cd("/Users/coope/OneDrive/Documents/Uni/Phylogenetics_coding/importance_scripts")
-include("/home/simoneb/Desktop/JMMenura/src/JMMenura.jl")
+##include("/home/simoneb/Desktop/JMMenura/src/JMMenura.jl")
 cd("/home/simoneb/Desktop/JMMenura")
-Pkg.develop(path="/home/simoneb/Desktop/JMMenura")
+## Pkg.develop(path="/home/simoneb/Desktop/JMMenura")
 using Phylo, Distributions, Pkg, Plots, DataFrames, XLSX, StatsBase, JLD2, LinearAlgebra, DifferentialEquations
 using PosDefManifold, ProgressMeter, StatsPlots, Random, Distributions
-using .JMMenura
+using JMMenura
 
 ##################################
 # Load reference simulation data #
